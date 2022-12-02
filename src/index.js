6afd43e4e88961312518aa2b32c0d346a018aca5
Javivy <84439052+Javivy@users.noreply.github.com>
2022-12-02 02:36:50 -0400

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.js';
