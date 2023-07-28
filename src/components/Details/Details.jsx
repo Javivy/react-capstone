3f6938d9a7850481b306af28005be665a4d837cc
Javier Vargas <Javieralejandroortga@gmail.com>
2023-07-28 05:46:30 -0400

    Build project

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
